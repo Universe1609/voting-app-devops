484e6c945211b84eecdbdbe04921a35cab86d60d
universe <davidrolandoaguirre@gmail.com>
2024-06-03 09:41:58 -0500

    change paht /result

--- Application-code/result/server.js
+++ Application-code/result/server.js
@@ -1,37 +1,36 @@
-var express = require('express'),
-    async = require('async'),
-    { Pool } = require('pg'),
-    cookieParser = require('cookie-parser'),
-    app = express(),
-    server = require('http').Server(app),
-    io = require('socket.io')(server);
+var express = require("express"),
+  async = require("async"),
+  { Pool } = require("pg"),
+  cookieParser = require("cookie-parser"),
+  app = express(),
+  server = require("http").Server(app),
+  io = require("socket.io")(server);
 
 var port = process.env.PORT || 4000;
 
-io.on('connection', function (socket) {
+io.on("connection", function (socket) {
+  socket.emit("message", { text: "Welcome!" });
 
-  socket.emit('message', { text : 'Welcome!' });
-
-  socket.on('subscribe', function (data) {
+  socket.on("subscribe", function (data) {
     socket.join(data.channel);
   });
 });
 
 var pool = new Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
+  connectionString: "postgres://postgres:postgres@db/postgres",
 });
 
 async.retry(
-  {times: 1000, interval: 1000},
-  function(callback) {
-    pool.connect(function(err, client, done) {
+  { times: 1000, interval: 1000 },
+  function (callback) {
+    pool.connect(function (err, client, done) {
       if (err) {
         console.error("Waiting for db");
       }
       callback(err, client);
     });
   },
-  function(err, client) {
+  function (err, client) {
     if (err) {
       return console.error("Giving up");
     }
@@ -41,20 +40,26 @@ async.retry(
 );
 
 function getVotes(client) {
-  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
-    if (err) {
-      console.error("Error performing query: " + err);
-    } else {
-      var votes = collectVotesFromResult(result);
-      io.sockets.emit("scores", JSON.stringify(votes));
-    }
+  client.query(
+    "SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote",
+    [],
+    function (err, result) {
+      if (err) {
+        console.error("Error performing query: " + err);
+      } else {
+        var votes = collectVotesFromResult(result);
+        io.sockets.emit("scores", JSON.stringify(votes));
+      }
 
-    setTimeout(function() {getVotes(client) }, 1000);
-  });
+      setTimeout(function () {
+        getVotes(client);
+      }, 1000);
+    }
+  );
 }
 
 function collectVotesFromResult(result) {
-  var votes = {a: 0, b: 0};
+  var votes = { a: 0, b: 0 };
 
   result.rows.forEach(function (row) {
     votes[row.vote] = parseInt(row.count);
@@ -65,13 +70,13 @@ function collectVotesFromResult(result) {
 
 app.use(cookieParser());
 app.use(express.urlencoded());
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + "/views"));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname + '/views/index.html'));
+app.get("/result", function (req, res) {
+  res.sendFile(path.resolve(__dirname + "/views/index.html"));
 });
 
 server.listen(port, function () {
   var port = server.address().port;
-  console.log('App running on port ' + port);
+  console.log("App running on port " + port);
 });
